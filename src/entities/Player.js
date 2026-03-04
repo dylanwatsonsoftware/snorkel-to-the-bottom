ee24a1a769fbd9de9fc666611dba6dc727136bde
Dylan Watson <lotsabackscatter@gmail.com>
2026-03-04 21:38:17 +0800

    Add invicibility frames

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -11,14 +11,17 @@ export class Player extends Phaser.GameObjects.Sprite {
 
         this.scene = scene;
         this.isSwinging = false;
+        this.health = 3;
+        this.isInvincible = false;
         this.setupSword();
     }
 
     setupSword() {
         this.sword = this.scene.add.container(0, 0).setAlpha(0).setDepth(10);
 
-        const bladeTop = this.scene.add.rectangle(10, -2, 45, 3, 0xeeeeee).setOrigin(0, 0.5);
-        const bladeBottom = this.scene.add.rectangle(10, 1, 40, 3, 0xcccccc).setOrigin(0, 0.5);
+        // Slightly longer blade for wider arc
+        const bladeTop = this.scene.add.rectangle(10, -2, 55, 3, 0xeeeeee).setOrigin(0, 0.5);
+        const bladeBottom = this.scene.add.rectangle(10, 1, 50, 3, 0xcccccc).setOrigin(0, 0.5);
         const hilt = this.scene.add.rectangle(8, 0, 4, 14, 0x8b4513).setOrigin(0.5, 0.5);
         const guard = this.scene.add.circle(5, 0, 6, 0xffd700).setAlpha(0.8);
         const handle = this.scene.add.rectangle(0, 0, 10, 5, 0x333333).setOrigin(0.5, 0.5);
@@ -26,7 +29,8 @@ export class Player extends Phaser.GameObjects.Sprite {
         this.sword.add([bladeTop, bladeBottom, guard, hilt, handle]);
 
         this.scene.physics.add.existing(this.sword);
-        this.sword.body.setSize(50, 15);
+        // Wider hitbox to match the swing arc
+        this.sword.body.setSize(60, 60);
         this.sword.body.setAllowGravity(false);
         this.sword.body.setImmovable(true);
     }
@@ -59,6 +63,9 @@ export class Player extends Phaser.GameObjects.Sprite {
         if (this.isSwinging) {
             this.sword.x = this.x;
             this.sword.y = this.y;
+            // Sync hitbox position
+            this.sword.body.x = this.x - 30;
+            this.sword.body.y = this.y - 30;
         }
     }
 
@@ -69,13 +76,18 @@ export class Player extends Phaser.GameObjects.Sprite {
         this.sword.setAlpha(1);
         this.sword.x = this.x;
         this.sword.y = this.y;
-        this.sword.setAngle(this.flipX ? 180 : 0);
+
+        // Start angle for a wide arc (e.g., -80 to +80 relative to facing)
+        const startAngle = this.flipX ? 100 : -80;
+        const endAngle = this.flipX ? 260 : 80;
+
+        this.sword.setAngle(startAngle);
 
         this.scene.tweens.add({
             targets: this.sword,
-            angle: this.flipX ? 270 : 90,
-            duration: 150,
-            ease: 'Power2',
+            angle: endAngle,
+            duration: 200,
+            ease: 'Cubic.out',
             onUpdate: () => {
                 this.sword.x = this.x;
                 this.sword.y = this.y;
@@ -85,5 +97,29 @@ export class Player extends Phaser.GameObjects.Sprite {
                 this.isSwinging = false;
             }
         });
+    }
+
+    takeDamage() {
+        if (this.isInvincible || this.health <= 0) return;
+
+        this.health--;
+        this.isInvincible = true;
+
+        // Flicker effect
+        this.scene.tweens.add({
+            targets: this,
+            alpha: 0.2,
+            duration: 100,
+            yoyo: true,
+            repeat: 10,
+            onComplete: () => {
+                this.alpha = 1;
+                this.isInvincible = false;
+            }
+        });
+
+        if (this.health <= 0) {
+            this.scene.gameOver();
+        }
     }
 }
